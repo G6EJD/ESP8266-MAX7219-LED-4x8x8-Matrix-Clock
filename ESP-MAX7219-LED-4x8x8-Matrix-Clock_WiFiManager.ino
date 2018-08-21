ff0640b9f429fd3f2a976bfc6acb3dceb166b057
G6EJD <d.l.bird@sky.com>
2018-08-21 15:43:37 +0100

    Add files via upload

--- ESP-MAX7219-LED-4x8x8-Matrix-Clock_WiFiManager.ino
+++ ESP-MAX7219-LED-4x8x8-Matrix-Clock_WiFiManager.ino
@@ -1,6 +1,10 @@
 #ifdef ESP32
+  #include <DNSServer.h>
+  #include <WebServer.h>
   #include <WiFi.h>
 #else
+  #include <DNSServer.h>
+  #include <ESP8266WebServer.h>
   #include <ESP8266WiFi.h>
 #endif
 #include <WiFiManager.h> 
